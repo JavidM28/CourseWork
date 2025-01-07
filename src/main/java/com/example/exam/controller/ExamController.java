f8986386c80b8f440dc95c6d1bb0b443b66a07f2
Javid <sairexm@gmail.com>
2025-01-07 20:44:15 +0400

    Final Commit

--- src/main/java/com/example/exam/controller/ExamController.java
+++ src/main/java/com/example/exam/controller/ExamController.java
@@ -1,4 +1,22 @@
 package com.example.exam.controller;
 
+import com.example.exam.model.Question;
+import com.example.exam.service.ExaminerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/exam/get")
 public class ExamController {
+    private final ExaminerService service;
+
+    public ExamController(ExaminerService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{amount}")
+    public Collection<Question> getQuestions(@PathVariable int amount) {
+        return service.getQuestions(amount);
+    }
 }
--- src/main/java/com/example/exam/controller/JavaQuestionController.java
+++ src/main/java/com/example/exam/controller/JavaQuestionController.java
@@ -1,4 +1,32 @@
 package com.example.exam.controller;
 
+import com.example.exam.model.Question;
+import com.example.exam.service.JavaQuestionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/exam/java")
 public class JavaQuestionController {
+    private final JavaQuestionService service;
+
+    public JavaQuestionController(JavaQuestionService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Collection<Question> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping("/add")
+    public Question addQuestion(@RequestParam String question, @RequestParam String answer) {
+        return service.addQuestion(question, answer);
+    }
+
+    @DeleteMapping("/remove")
+    public Question removeQuestion(@RequestParam String question, @RequestParam String answer) {
+        return service.removeQuestion(question, answer);
+    }
 }
